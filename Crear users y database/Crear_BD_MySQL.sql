467032cbd1f00e259c0b3eee665ddbb19645ab79
Joel <joelruizm06@gmail.com>
2025-12-01 23:57:58 +0100

    Ejercicios base de datos

---
+++ Crear users y database/Crear_BD_MySQL.sql
@@ -0,0 +1,3 @@
+create database bol2ejer1;
+-- create user 'dummy'@'%' identified by 'dummy';
+-- grant all on db_exampleName.* to 'dummy'@'%';
---
+++ Crear users y database/crear_usuario.sql
@@ -0,0 +1,3 @@
+alter session set "_oracle_script"=true;  
+create user bol1ejer7 identified by bol1ejer7;
+GRANT CONNECT, RESOURCE, DBA TO bol1ejer7;
