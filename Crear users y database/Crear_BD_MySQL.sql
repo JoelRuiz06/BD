012f7a9844b16bc714caf11d63a277c6fbac5726
Joel <joelruizm06@gmail.com>
2026-01-29 22:52:26 +0100

    Actualizacion

--- Crear users y database/Crear_BD_MySQL.sql
+++ Crear users y database/Crear_BD_MySQL.sql
@@ -1,3 +1,3 @@
-create database ProyectoGrupo;
+create database T4BOL2EJER3;
 -- create user 'dummy'@'%' identified by 'dummy';
 -- grant all on db_exampleName.* to 'dummy'@'%';
--- Crear users y database/crear_usuario.sql
+++ Crear users y database/crear_usuario.sql
@@ -1,3 +1,3 @@
 alter session set "_oracle_script"=true;  
-create user ProyectoGrupo identified by ProyectoGrupo;
-GRANT CONNECT, RESOURCE, DBA TO ProyectoGrupo;
+create user SimulacroExamen identified by SimulacroExamen;
+GRANT CONNECT, RESOURCE, DBA TO SimulacroExamen;
