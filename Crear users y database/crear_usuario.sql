9e0d02fd51650785180ec88fe40b410faf9ad75c
Joel <joelruizm06@gmail.com>
2026-02-20 09:05:13 +0100

    actualizado

--- Crear users y database/crear_usuario.sql
+++ Crear users y database/crear_usuario.sql
@@ -1,3 +1,3 @@
 alter session set "_oracle_script"=true;  
-create user SimulacroExamen identified by SimulacroExamen;
-GRANT CONNECT, RESOURCE, DBA TO SimulacroExamen;
+create user T5EJ1 identified by T5EJ1;
+GRANT CONNECT, RESOURCE, DBA TO T5EJ1;
