7f32b0a8797a8e524a0153d0e511293419f5a6df
Joel <joelruizm06@gmail.com>
2026-02-25 09:28:09 +0100

    modificaciones

--- Crear users y database/Crear_BD_MySQL.sql
+++ Crear users y database/Crear_BD_MySQL.sql
@@ -1,3 +1,3 @@
-create database T4BOL2EJER3;
+create database T5EJER1REFUERZO;
 -- create user 'dummy'@'%' identified by 'dummy';
 -- grant all on db_exampleName.* to 'dummy'@'%';
--- Crear users y database/crear_usuario.sql
+++ Crear users y database/crear_usuario.sql
@@ -1,3 +1,3 @@
 alter session set "_oracle_script"=true;  
-create user T5EJ1 identified by T5EJ1;
-GRANT CONNECT, RESOURCE, DBA TO T5EJ1;
+create user T5EJUniversidad identified by T5EJUniversidad;
+GRANT CONNECT, RESOURCE, DBA TO T5EJUniversidad;
